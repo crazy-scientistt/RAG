dd497cd60c6e0e0e719d04585f4f5a079e5f0923
Abdulrehman Qureshi <135824746+crazy-scientistt@users.noreply.github.com>
2026-02-08 04:49:56 +0300

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 // Frontend Configuration
 window.FRONTEND_CONFIG = {
     // Backend API URL - change this to match your backend location
-    API_URL: 'http://localhost:8000'
+    API_URL: 'rag-production-698f.up.railway.app'
 };
